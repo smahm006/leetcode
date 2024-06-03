9ebff6fe8a2e7be8246b50d73577e39bd88e754c
Sohaib Mahmood <soh.mahmood@fastmail.com>
2024-06-02 22:32:58 -0400

    Completed Leetcode problem 125 with python

---
+++ 125_valid_palindrome/main.py
@@ -0,0 +1,9 @@
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub(r'[^a-zA-Z0-9]', '', s.lower())
+        return s == s[::-1]
+
+sol = Solution()
+test = "A man, a plan, a canal: Panama"
+print(sol.isPalindrome(test))
